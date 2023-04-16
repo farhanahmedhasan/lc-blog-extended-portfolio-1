300f703fca8ddee482d43ff08e97039a6b810590
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-04-16 13:21:08 +0600

    Added mailchimp and playing with it

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,22 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ping', function(){
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us8'
+    ]);
+
+    $response = $mailchimp->lists->addListMember("c4ee03525e",[
+        "email_address" => "azhasandjmamunxyz2015@gmail.com",
+        "status" => "subscribed",
+    ]);
+    
+    dd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::post('/posts/{post:slug}/comments', [CommentController::class, 'store']);
