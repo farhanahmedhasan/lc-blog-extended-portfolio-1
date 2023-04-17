9dbe16b977650267ef0a13be915f4adb97eec192
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-04-17 20:52:23 +0600

    Added Newsletter Interface and added this to service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Providers;
+
+use MailchimpMarketing\ApiClient;
+use App\Services\Newsletter;
+use App\Services\MailchimpNewsletter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -10,7 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient())->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us8'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
---
+++ app/Services/ConvertKitNewsletter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+class ConvertKitNewsletter implements Newsletter{
+    public function subscribe(){
+        //Subscribe user with ConvertKit specefic API Requests
+    }
+}
