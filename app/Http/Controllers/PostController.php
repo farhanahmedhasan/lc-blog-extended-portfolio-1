948e978bb2a3cedb2079439a2195836b08de747a
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-04-18 14:08:28 +0600

    Added admin middleware so that only admin can post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,4 +19,9 @@ class PostController extends Controller
     {
         return view('posts.show', ['post'=> $post]);
     }
+
+    public function Create(): View
+    {
+        return view('posts.create');
+    }
 }
