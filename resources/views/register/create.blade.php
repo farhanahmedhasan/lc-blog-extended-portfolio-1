dace4741d4aabc8023a9b50cd03fee3676e23f91
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-04-21 16:57:05 +0600

    Refactor register and session inputs

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -5,72 +5,11 @@
 
             <form method="POST"  action="/register">
                 @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
-                        name
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           id="name"
-                           name="name"
-                           value="{{old('name')}}"
-                           required
-                    />
-
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
-                        Username
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           id="username"
-                           name="username"
-                           value="{{old('username')}}"
-                           required
-                    />
-
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
-                        Email
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="email"
-                           id="email"
-                           name="email"
-                           value="{{old('email')}}"
-                           required
-                    />
-
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
-                        Password
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="password"
-                           id="password"
-                           name="password"
-                           required
-                    />
-
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
-                </div>
+                
+                <x-form.input name="name"/>
+                <x-form.input name="username"/>
+                <x-form.input name="email" type="email"/>
+                <x-form.input name="password" type="password"/>
 
                 <div class="mb-6">
                     <button type="submit" class="bg-blue-500 text-white rounded py-2 px-4 hover:bg-blue-600" >
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -6,38 +6,8 @@
             <form method="POST"  action="/login">
                 @csrf
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
-                        Email
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="email"
-                           id="email"
-                           name="email"
-                           value="{{old('email')}}"
-                           required
-                    />
-
-                    @error('email')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
-                        Password
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="password"
-                           id="password"
-                           name="password"
-                           required
-                    />
-
-                    @error('password')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
-                </div>
+                <x-form.input name="email" type="email"/>
+                <x-form.input name="password" type="password"/>
 
                 <div class="mb-6">
                     <button type="submit" class="bg-blue-500 text-white rounded py-2 px-4 hover:bg-blue-600" >
